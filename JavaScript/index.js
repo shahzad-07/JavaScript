e69dfe1b7c1a6027165e01e709d9c1cdd3220843
Mohammed Shahzadul Quadri <73131875+shahzad-07@users.noreply.github.com>
2024-03-07 12:55:17 +0530

    Delete JavaScript directory

--- JavaScript/index.js
+++
@@ -1,7 +0,0 @@
-let username;
-
-document.getElementById("myButton").onclick = function() {
-    username = document.getElementById("myText").value;
-    console.log("My name is : " , username);
-    document.getElementById("myLabel").innerHTML = "Hello "+username;
-}
